2efbe0dd87300f04eb2da2bf94a5568c425c06c8
mmazurekgda <mmazurekgda@gmail.com>
2015-05-25 17:13:02 +0200

    kk

--- Aplikacja/Form1.h
+++ Aplikacja/Form1.h
@@ -34,14 +34,39 @@ namespace WindowsFormApplication1 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::DataVisualization::Charting::Chart^  chart1;
+
+
+
+
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::BindingNavigator^  bindingNavigator1;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorAddNewItem;
+	private: System::Windows::Forms::ToolStripLabel^  bindingNavigatorCountItem;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorDeleteItem;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorMoveFirstItem;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorMovePreviousItem;
+	private: System::Windows::Forms::ToolStripSeparator^  bindingNavigatorSeparator;
+	private: System::Windows::Forms::ToolStripTextBox^  bindingNavigatorPositionItem;
+	private: System::Windows::Forms::ToolStripSeparator^  bindingNavigatorSeparator1;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorMoveNextItem;
+	private: System::Windows::Forms::ToolStripButton^  bindingNavigatorMoveLastItem;
+	private: System::Windows::Forms::ToolStripSeparator^  bindingNavigatorSeparator2;
+	private: System::ComponentModel::IContainer^  components;
+
+
+
+
+
+
+
+
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -50,42 +75,171 @@ namespace WindowsFormApplication1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Windows::Forms::DataVisualization::Charting::ChartArea^  chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
-			System::Windows::Forms::DataVisualization::Charting::Legend^  legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
-			System::Windows::Forms::DataVisualization::Charting::Series^  series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
-			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->bindingNavigator1 = (gcnew System::Windows::Forms::BindingNavigator(this->components));
+			this->bindingNavigatorAddNewItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorCountItem = (gcnew System::Windows::Forms::ToolStripLabel());
+			this->bindingNavigatorDeleteItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMoveFirstItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMovePreviousItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorSeparator = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->bindingNavigatorPositionItem = (gcnew System::Windows::Forms::ToolStripTextBox());
+			this->bindingNavigatorSeparator1 = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->bindingNavigatorMoveNextItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMoveLastItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorSeparator2 = (gcnew System::Windows::Forms::ToolStripSeparator());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingNavigator1))->BeginInit();
+			this->bindingNavigator1->SuspendLayout();
 			this->SuspendLayout();
 			// 
-			// chart1
+			// label1
 			// 
-			chartArea1->Name = L"ChartArea1";
-			this->chart1->ChartAreas->Add(chartArea1);
-			legend1->Name = L"Legend1";
-			this->chart1->Legends->Add(legend1);
-			this->chart1->Location = System::Drawing::Point(27, 23);
-			this->chart1->Name = L"chart1";
-			series1->ChartArea = L"ChartArea1";
-			series1->Legend = L"Legend1";
-			series1->Name = L"Series1";
-			this->chart1->Series->Add(series1);
-			this->chart1->Size = System::Drawing::Size(307, 168);
-			this->chart1->TabIndex = 0;
-			this->chart1->Text = L"chart1";
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Aleo", 26.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(349, 17);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(561, 42);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Metoda najmniejszych kwadratów";
+			// 
+			// bindingNavigator1
+			// 
+			this->bindingNavigator1->AddNewItem = this->bindingNavigatorAddNewItem;
+			this->bindingNavigator1->CountItem = this->bindingNavigatorCountItem;
+			this->bindingNavigator1->DeleteItem = this->bindingNavigatorDeleteItem;
+			this->bindingNavigator1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(11) {
+				this->bindingNavigatorMoveFirstItem,
+					this->bindingNavigatorMovePreviousItem, this->bindingNavigatorSeparator, this->bindingNavigatorPositionItem, this->bindingNavigatorCountItem,
+					this->bindingNavigatorSeparator1, this->bindingNavigatorMoveNextItem, this->bindingNavigatorMoveLastItem, this->bindingNavigatorSeparator2,
+					this->bindingNavigatorAddNewItem, this->bindingNavigatorDeleteItem
+			});
+			this->bindingNavigator1->Location = System::Drawing::Point(0, 0);
+			this->bindingNavigator1->MoveFirstItem = this->bindingNavigatorMoveFirstItem;
+			this->bindingNavigator1->MoveLastItem = this->bindingNavigatorMoveLastItem;
+			this->bindingNavigator1->MoveNextItem = this->bindingNavigatorMoveNextItem;
+			this->bindingNavigator1->MovePreviousItem = this->bindingNavigatorMovePreviousItem;
+			this->bindingNavigator1->Name = L"bindingNavigator1";
+			this->bindingNavigator1->PositionItem = this->bindingNavigatorPositionItem;
+			this->bindingNavigator1->Size = System::Drawing::Size(1213, 25);
+			this->bindingNavigator1->TabIndex = 3;
+			this->bindingNavigator1->Text = L"bindingNavigator1";
+			// 
+			// bindingNavigatorAddNewItem
+			// 
+			this->bindingNavigatorAddNewItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorAddNewItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorAddNewItem.Image")));
+			this->bindingNavigatorAddNewItem->Name = L"bindingNavigatorAddNewItem";
+			this->bindingNavigatorAddNewItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorAddNewItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorAddNewItem->Text = L"Add new";
+			// 
+			// bindingNavigatorCountItem
+			// 
+			this->bindingNavigatorCountItem->Name = L"bindingNavigatorCountItem";
+			this->bindingNavigatorCountItem->Size = System::Drawing::Size(35, 22);
+			this->bindingNavigatorCountItem->Text = L"of {0}";
+			this->bindingNavigatorCountItem->ToolTipText = L"Total number of items";
+			// 
+			// bindingNavigatorDeleteItem
+			// 
+			this->bindingNavigatorDeleteItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorDeleteItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorDeleteItem.Image")));
+			this->bindingNavigatorDeleteItem->Name = L"bindingNavigatorDeleteItem";
+			this->bindingNavigatorDeleteItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorDeleteItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorDeleteItem->Text = L"Delete";
+			// 
+			// bindingNavigatorMoveFirstItem
+			// 
+			this->bindingNavigatorMoveFirstItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveFirstItem->Name = L"bindingNavigatorMoveFirstItem";
+			this->bindingNavigatorMoveFirstItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveFirstItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveFirstItem->Text = L"Move first";
+			// 
+			// bindingNavigatorMovePreviousItem
+			// 
+			this->bindingNavigatorMovePreviousItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMovePreviousItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMovePreviousItem.Image")));
+			this->bindingNavigatorMovePreviousItem->Name = L"bindingNavigatorMovePreviousItem";
+			this->bindingNavigatorMovePreviousItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMovePreviousItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMovePreviousItem->Text = L"Move previous";
+			// 
+			// bindingNavigatorSeparator
+			// 
+			this->bindingNavigatorSeparator->Name = L"bindingNavigatorSeparator";
+			this->bindingNavigatorSeparator->Size = System::Drawing::Size(6, 25);
+			// 
+			// bindingNavigatorPositionItem
+			// 
+			this->bindingNavigatorPositionItem->AccessibleName = L"Position";
+			this->bindingNavigatorPositionItem->AutoSize = false;
+			this->bindingNavigatorPositionItem->Name = L"bindingNavigatorPositionItem";
+			this->bindingNavigatorPositionItem->Size = System::Drawing::Size(50, 23);
+			this->bindingNavigatorPositionItem->Text = L"0";
+			this->bindingNavigatorPositionItem->ToolTipText = L"Current position";
+			// 
+			// bindingNavigatorSeparator1
+			// 
+			this->bindingNavigatorSeparator1->Name = L"bindingNavigatorSeparator1";
+			this->bindingNavigatorSeparator1->Size = System::Drawing::Size(6, 25);
+			// 
+			// bindingNavigatorMoveNextItem
+			// 
+			this->bindingNavigatorMoveNextItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveNextItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMoveNextItem.Image")));
+			this->bindingNavigatorMoveNextItem->Name = L"bindingNavigatorMoveNextItem";
+			this->bindingNavigatorMoveNextItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveNextItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveNextItem->Text = L"Move next";
+			// 
+			// bindingNavigatorMoveLastItem
+			// 
+			this->bindingNavigatorMoveLastItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveLastItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMoveLastItem.Image")));
+			this->bindingNavigatorMoveLastItem->Name = L"bindingNavigatorMoveLastItem";
+			this->bindingNavigatorMoveLastItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveLastItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveLastItem->Text = L"Move last";
+			// 
+			// bindingNavigatorSeparator2
+			// 
+			this->bindingNavigatorSeparator2->Name = L"bindingNavigatorSeparator2";
+			this->bindingNavigatorSeparator2->Size = System::Drawing::Size(6, 25);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(438, 261);
-			this->Controls->Add(this->chart1);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)));
+			this->ClientSize = System::Drawing::Size(1213, 619);
+			this->Controls->Add(this->bindingNavigator1);
+			this->Controls->Add(this->label1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingNavigator1))->EndInit();
+			this->bindingNavigator1->ResumeLayout(false);
+			this->bindingNavigator1->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void toolStripComboBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	}
+private: System::Void menuG³ówneToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+}
+private: System::Void menuStrip1_ItemClicked(System::Object^  sender, System::Windows::Forms::ToolStripItemClickedEventArgs^  e) {
+}
+private: System::Void dataGridView1_CellContentClick(System::Object^  sender, System::Windows::Forms::DataGridViewCellEventArgs^  e) {
+}
+
+};
 }
 
